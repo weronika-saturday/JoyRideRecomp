c9e4c7bbb1957195bb646830280516d7e46fa453
weronika-saturday <saturdayweronika@gmail.com>
2026-03-24 21:20:41 +0500

    Add Rexglue SDK config for Joy Ride Turbo

---
+++ JoyRideTurbo/src/main.cpp
@@ -0,0 +1,11 @@
+
+// joyrideturbo - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+
+#include "generated/joyrideturbo_config.h"
+#include "generated/joyrideturbo_init.h"
+
+#include "joyrideturbo_app.h"
+
+REX_DEFINE_APP(joyrideturbo, JoyrideturboApp::Create)
